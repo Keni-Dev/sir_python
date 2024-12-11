ed040ddf8d9c5bb7b20e7af60ba904bfbbe6ea80
Keni <100826816+Keni-Dev@users.noreply.github.com>
2024-12-12 07:45:28 +0800

    kengkong

---
+++ ZamoraKennyIvan_1.py
@@ -0,0 +1,31 @@
+# Problem No. 1: Duplicate the first number according to the value of the second number
+try: n1, n2 = map(int, input("Enter two numbers: ").split()); print(str(n1) * n2)
+except ValueError: print("Invalid input. Please enter integers.")
+
+# Problem No. 2: Display a rectangle
+try:
+    h = int(input("Enter height: ")); if h < 2: raise ValueError
+    print("+{}+".format("=" * 10)); [print("+          +") for _ in range(h - 2)]; print("+{}+".format("=" * 10))
+except ValueError: print("Invalid height. Enter an integer >= 2.")
+
+# Problem No. 3: Duplicate first number, calculate and sum numbers
+try:
+    n1, n2 = map(int, input("Enter two numbers: ").split())
+    print(f"The sum of all three numbers are {(n1 * n2) + n2 + (n2 * 6)}.")
+except ValueError: print("Invalid input. Please enter integers.")
+
+# Problem No. 4: Format integers and floats with proper type handling
+while(1):
+    try:
+        n1 = int(input("Enter an integer: "))
+        n2 = float(input("Enter a float: "))
+        print("First Number: %.2f, Second Number: %d" % (n1, n2))
+        break
+    except ValueError: 
+        print("Invalid input. Ensure the first is an integer and the second is a float.")
+        continue
+
+# Problem No. 5: Duplicate separator and ending characters 
+sep = input("Enter separator character/s: ")
+end = input("Enter ending character/s: ")
+print("I", "love", "Python", sep=sep*2, end=end*2)
