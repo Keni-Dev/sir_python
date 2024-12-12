1dd53c27deb4c2fda8ec0416069b673e32298929
Keni <100826816+Keni-Dev@users.noreply.github.com>
2024-12-12 12:58:44 +0800

    Kengkongg

--- ZamoraKennyIvan_1.py
+++ ZamoraKennyIvan_1.py
@@ -1,20 +1,31 @@
-# Problem No. 1: Duplicate the first number according to the value of the second number
+# Problem No. 1
 try: n1, n2 = map(int, input("Enter two numbers: ").split()); print(str(n1) * n2)
 except ValueError: print("Invalid input. Please enter integers.")
 
-# Problem No. 2: Display a rectangle
+#2 lines
+#n1, n2 = map(int, input("Enter two numbers: ").split())
+#print(str(n1) * n2)
+
+# Problem No. 2
 try:
-    h = int(input("Enter height: ")); if h < 2: raise ValueError
+    h = int(input("Enter height: "))
+    if h < 2: raise ValueError
     print("+{}+".format("=" * 10)); [print("+          +") for _ in range(h - 2)]; print("+{}+".format("=" * 10))
 except ValueError: print("Invalid height. Enter an integer >= 2.")
+# added error handling to continue program if the input is error
+#4 lines without error handling
+#h = int(input("Enter height: "))
+#print("+{}+".format("=" * 10))
+#for _ in range(h - 2): print("+          +")
+#print("+{}+".format("=" * 10))
 
-# Problem No. 3: Duplicate first number, calculate and sum numbers
+# Problem No. 3
 try:
     n1, n2 = map(int, input("Enter two numbers: ").split())
     print(f"The sum of all three numbers are {(n1 * n2) + n2 + (n2 * 6)}.")
 except ValueError: print("Invalid input. Please enter integers.")
 
-# Problem No. 4: Format integers and floats with proper type handling
+# Problem No. 4
 while(1):
     try:
         n1 = int(input("Enter an integer: "))
@@ -25,7 +36,7 @@ while(1):
         print("Invalid input. Ensure the first is an integer and the second is a float.")
         continue
 
-# Problem No. 5: Duplicate separator and ending characters 
+# Problem No. 5
 sep = input("Enter separator character/s: ")
 end = input("Enter ending character/s: ")
 print("I", "love", "Python", sep=sep*2, end=end*2)
